f5d130dec4f965984d4e6737760554d8b28c7b9e
Edwin Rodriguez <165713870+Creait01@users.noreply.github.com>
2026-01-26 17:59:05 -0400

    Delete cash_register_close directory

--- cash_register_close/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import cash_register_close_wizard
-from . import cash_register_mass_close_wizard
-from . import cash_register_mass_confirm_wizard
-from . import cash_register_copy_previous_wizard
